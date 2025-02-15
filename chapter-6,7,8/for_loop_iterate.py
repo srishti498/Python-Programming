cb13083cc71883a80416b531715dba80f70c5b6d
Srishti Kumari <kumarsinghnarendra081@gmail.com>
2025-02-15 12:22:44 +0530

    first commit

---
+++ chapter-6,7,8/for_loop_iterate.py
@@ -0,0 +1,15 @@
+#for loops with lists
+l = [1, 4, 6, 345, 3, 768]
+
+for i in l:
+    print(i)
+
+# for loops with tuples
+t = (6, 231, 75, 122)
+for i in t:
+    print(i)    
+    
+# for loops with strings
+s = "Harry"
+for i in s:
+    print(i) 
---
+++ chapter-6,7,8/pass.py
@@ -0,0 +1,7 @@
+for i in range(645):
+    pass #its instructs to do nothing
+
+i = 0
+while(i<45):
+    print(i)
+    i += 1   
